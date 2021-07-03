ffab77f5458b52ebe619c6c9dcaecf42cab8fb08
Duane McDonald <mcdonald.duane13@gmail.com>
2021-07-03 06:24:41 -0400

    rm next.config env public_url

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
 module.exports = {
-  env: {
-    PUBLIC_URL: ''
-  },
   experimental: {
-    craCompat: true,
+    craCompat: false,
   },
   // Remove this to leverage Next.js' static image handling
   // read more here: https://nextjs.org/docs/api-reference/next/image
   images: {
-    disableStaticImages: true
-  }  
-}
+    disableStaticImages: false,
+  },
+};
